5635816ca85f759990620d8be63dd2518b1103b8
AlexeyZh <waltafunk@gmail.com>
2023-04-25 12:32:13 +0300

    refactor

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,14 +1,12 @@
-from selenium.webdriver.common.by import By
-
-from pages.base_page import BasePage
+from pages.SeleniumPage import SeleniumPage
 
 
 def test_example(driver):
-    page = BasePage(driver, ' http://selenium.dev/')
+    page = SeleniumPage(driver)
     page.open()
 
     # Locate the child text element within the svg element
-    el = page.element_is_visible((By.CSS_SELECTOR, "svg[id$='1']"))
+    el = page.find_svg_element("1")
 
     # Print the text of the child element
     print(el.text)
