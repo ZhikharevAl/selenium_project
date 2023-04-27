83251ab99c6f6dadc592b4a4749e4bc37173f433
AlexeyZh <waltafunk@gmail.com>
2023-04-27 16:32:13 +0300

    add @pytest.fixture(scope="function")     def login(driver)

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,9 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+from pages.your_name_page import YourNamePage
+
+
 @pytest.fixture(scope="function")
 def driver():
     options = Options()
@@ -14,3 +17,12 @@ def driver():
     driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
+
+
+@pytest.fixture(scope="function")
+def login(driver):
+    page_your_name = YourNamePage(driver)
+    page_your_name.open()
+    page_your_name.select_your_name()
+    page_your_name.click_login_button()
+
