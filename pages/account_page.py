ebf041f66985f50a399b503a3791884aa05bde78
AlexeyZh <waltafunk@gmail.com>
2023-04-27 13:13:30 +0300

    add button selector

--- pages/account_page.py
+++ pages/account_page.py
@@ -7,6 +7,12 @@ class AccountPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver, self.URL)
         self.welcome_text = "span[class^='fontBig']"
+        self.button_transactions = "button[ng-class='btnClass1']"
+        self.button_deposit = "button[ng-class='btnClass2']"
+        self.button_withdrawl = "button[ng-class='btnClass3']"
 
     def get_element_text(self):
         return self.get_text(self.welcome_text)
+
+    def click_button_transactions(self):
+        self.click_button(self.button_transactions)
